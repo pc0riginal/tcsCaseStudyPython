0b382c9bf5bc7e0b14d3422fb3eff0fce8c3c8b9
msm1998 <pc.pramukh99@gmail.com>
2020-06-13 13:37:58 +0530

    forms added

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,28 @@
-from app import app
+from app import app,mongo
+from flask import render_template,redirect,url_for,flash,session
+from app.forms import LoginForm
+
+user = mongo.db['user']
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return 'Hello World'
+    if not session.get("username"):
+        return redirect('/login')
+    return render_template("index.html")
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if session.get("username"):
+        return redirect("/index")
+    form = LoginForm()
+    if form.validate_on_submit():
+        userData = user.find_one({"username":form.username.data})
+        if userData and userData['password'] == form.password.data:
+            flash(f"{userData['username']},you are successfully logged in!","success")
+            session['username'] = userData['username']
+            return redirect("/index")
+        else:
+            flash("sorry try again!","danger")
+    return render_template("login.html",login=True,title="Login",form=form)        
+
