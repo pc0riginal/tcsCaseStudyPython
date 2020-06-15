ea287ff8eff6be3b61585a0ae2fb8a114f684b9b
msm1998 <pc.pramukh99@gmail.com>
2020-06-15 22:16:05 +0530

    forms added

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,35 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
-from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm,RecaptchaField
+from wtforms import StringField,PasswordField,SubmitField,SelectField,IntegerField
+from wtforms.validators import DataRequired,Required,NumberRange,Regexp,Length
 
 class LoginForm(FlaskForm):
     username = StringField(label="Username",validators=[DataRequired()])
     password = PasswordField(label="Password",validators=[DataRequired()])
     submit = SubmitField(label="Login")
+
+accounts = [('Saving','saving'),('Current','current')]
+class CreateAccount(FlaskForm):
+    customerID = StringField(label="Customer ID(SSN ID)",validators=[DataRequired(),Regexp('^\\d{9}$',message="required 9 digit ID")])
+    account_type = SelectField(label="Account Type",choices=accounts,validators=[Required()])
+    amount = IntegerField(label="Amount",validators=[DataRequired(),NumberRange(min=0)])
+    submit = SubmitField(label="Create")
+
+class DeleteAccount(FlaskForm):
+    accountID = StringField(label="Account ID",validators=[DataRequired()])
+    account_type = SelectField(label="Account Type",choices=accounts,validators=[Required()])
+    submit = SubmitField(label="Delete")
+
+class CreateCustomer(FlaskForm):
+    customerID = StringField(label="Customer ID(SSN ID)",validators=[DataRequired(),Regexp('^\\d{9}$',message="required 9 digit ID")])
+    name = StringField(label="Name",validators=[DataRequired(),Length(min=3)])
+    age = IntegerField(label="Age",validators=[DataRequired(),NumberRange(min=10,max=100)])
+    addressline1 = StringField('Address Line 1*',validators=[DataRequired(), Length(min=1, max=100)])
+    addressline2 = StringField('Address Line 2',validators=[Length(max=100)])
+    city = StringField('City',validators=[DataRequired()])
+    state = StringField('State',validators=[DataRequired()])
+    submit = SubmitField(label="Create")
+
+class UpdateCustomer(FlaskForm):
+    customerID = StringField(label="Customer ID(SSN ID)",validators=[DataRequired(),Regexp('^\\d{9}$',message="required 9 digit ID")])
+    submit = SubmitField(label="Update")
+    
