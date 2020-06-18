097a0225cd5730152f022a213a2ceff8d2ca2435
msm1998 <pc.pramukh99@gmail.com>
2020-06-18 10:46:36 +0530

    added

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 class Config(object):
-    SECRET_KEY = os.environ.get("SECRET_KEY") or os.urandom(16)
+    SECRET_KEY = os.environ.get("SECRET_KEY") or b'_5#y2L"F4Q8z\n\xec]/'
     MONGO_DBNAME = "Bank"
     MONGO_URI = "mongodb+srv://msm98:paras123@cluster0-4gnmc.mongodb.net/Bank?retryWrites=true&w=majority"
